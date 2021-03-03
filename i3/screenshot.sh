efef97eb26bdfddf8896fdbb2239256cf5bdb85b
photosheep <remzeh2@gmail.com>
2021-03-03 15:28:44 -0500

    Music alerts, notification server and i3 cleanups.

--- i3/screenshot.sh
+++ i3/screenshot.sh
@@ -2,5 +2,7 @@
 
 if [ -z ${1} ]; then flags='u'; else flags=$1;fi
 file=~/Captures/$(date '+%F_%H%M%S').jpg
-maim -$flags $file
-notify-send -i $file "Screenshot taken!" "File is $file"
+if $(maim -$flags $file);
+then
+    notify-send -i $file "Screenshot taken!" "File is $file"
+fi
--- pianobar/notify-pb.sh
+++ pianobar/notify-pb.sh
@@ -13,9 +13,9 @@ tmpsonginfo="/tmp/pianobar_info"
 
 notify () {
     if [[ -n $tmpart ]]; then
-        notify-send -i $tmpart "pianobar | $1" "$2"
+        dunstify -h string:x-dunst-stack-tag:pianobar -i $tmpart "$1" "$2"
     else
-        notify-send -i /usr/share/icons/Luv/devices/16/multimedia-player.svg "pianobar | $1" "$2"
+        dunstify -h string:x-dunst-stack-tag:pianobar -i /usr/share/icons/Luv/devices/16/multimedia-player.svg "$1" "$2"
     fi
 }
 
@@ -30,11 +30,11 @@ case "$1" in
         [[ $rating == "1" ]] && ratestr="" || ratestr=""
         if [[ -n "$coverArt" ]]; then
             wget -q -O "$tmpart" "$coverArt"
-            convert $tmpart -resize 100x100 $tmpart
+#            convert $tmpart -resize 100x100 $tmpart
         else
             [[ -n $tmpart ]] && rm $tmpart
         fi
-        notify "Station : $stationName" "$title - $artist on $album $ratestr"
+        notify "$stationName" "\n<b>$title</b> $ratestr\nby..\n<b>$artist</b>\n<i>$album</i>"
         rm -f $tmpstations
         for stnum in $(seq 0 $(($stationCount-1))); do
              echo "$stnum) "$(eval "echo \$station$stnum") >> "$tmpstations"
